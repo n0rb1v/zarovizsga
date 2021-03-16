08173e65661fde4f154088cb7583adfa34a66ed0
n0rb1v <n0rb1v@gmail.com>
2021-03-16 19:55:47 +0100

    1 jav

--- src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
@@ -1,7 +1,9 @@
 package hu.nive.ujratervezes.zarovizsga.digitscounter;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class DigitsCounter {
@@ -9,10 +11,12 @@ public class DigitsCounter {
         if (s == null) {
             return 0;
         }
-
+        List<Character> chars = List.of('0','1','2','3','4','5','6','7','8','9');
         Set<Character> ss = new HashSet<>();
         for (int i = 0; i < s.length(); i++) {
-            ss.add(s.charAt(i));
+            if (chars.contains(s.charAt(i))){
+                ss.add(s.charAt(i));
+            }
         }
         return ss.size();
     }
