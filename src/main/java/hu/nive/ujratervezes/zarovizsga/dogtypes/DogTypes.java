239b5f46cef5117e951f397e2696ebbc102c8933
n0rb1v <n0rb1v@gmail.com>
2021-03-16 19:40:53 +0100

    4

--- src/main/java/hu/nive/ujratervezes/zarovizsga/dogtypes/DogTypes.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/dogtypes/DogTypes.java
@@ -1,4 +1,40 @@
 package hu.nive.ujratervezes.zarovizsga.dogtypes;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DogTypes {
+    private DataSource ds;
+
+    public DogTypes(DataSource ds) {
+        this.ds = ds;
+    }
+
+    public List<String> getDogsByCountry(String country) {
+        try (Connection conn = ds.getConnection();
+             PreparedStatement stmt =
+                     conn.prepareStatement(
+                             "SELECT name FROM dog_types WHERE country = ?")) {
+            stmt.setString(1, country);
+            List<String> x = getStatement(stmt);
+            return x;
+        } catch (SQLException e) {
+            throw new IllegalStateException("select error");
+        }
+    }
+
+    private List<String> getStatement(PreparedStatement stmt) throws SQLException {
+        List<String> result = new ArrayList<>();
+        try (ResultSet rs = stmt.executeQuery()) {
+            while (rs.next()) {
+                result.add(rs.getString(1).toLowerCase());
+            }
+        }
+        return result;
+    }
 }
